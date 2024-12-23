242a5fc9d0b168e5c6a50ee6420029084726d773
Hadi Rahmaninejad <rahmanin@discovery1.usc.edu>
2024-12-22 17:43:04 -0800

    revising the notebooks

--- OccupancyInputCTCF/utils/models.py
+++ OccupancyInputCTCF/utils/models.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Input
 from tensorflow.keras.optimizers import Adam
-
+from tensorflow.keras.regularizers import l2
 
 
 def model_linear_reg(features, learningrate):
@@ -18,4 +18,29 @@ def model_logistic_reg(features, learningrate):
     model.add(Flatten())
     model.add(Dense(1, activation='sigmoid')) 
     model.compile(optimizer= Adam(learning_rate = learningrate), loss = 'mean_squared_error')
+    return model
+
+def model_logistic_regression(features, learningrate, regularization_strength=0.01):
+    model = Sequential()
+    model.add(Input(shape=(features.shape[1], features.shape[2])))
+    model.add(Flatten())
+    model.add(Dense(1, activation='sigmoid', kernel_regularizer=l2(regularization_strength))) 
+    model.compile(optimizer=Adam(learning_rate=learningrate), loss='mean_squared_error')
+    return model
+
+
+
+
+
+def Regressionmodel(features, learningrate, dense_layers = 1, activation=None, regularization_strength=0.01):
+    model = Sequential()
+    model.add(Input(shape=(features.shape[1], features.shape[2])))
+    model.add(Flatten())
+    
+    # Add the dense layer with the given activation and regularization (if specified)
+    model.add(Dense(dense_layers, activation=activation, kernel_regularizer=l2(regularization_strength))) 
+    
+    # Compile the model with MSE loss for continuous target values
+    model.compile(optimizer=Adam(learning_rate=learningrate), loss='mean_squared_error')
+    
     return model
