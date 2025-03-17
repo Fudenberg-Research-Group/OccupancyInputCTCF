a3b25b8aa192ee41fa4a404e935db3a6aad956f3
Hadi Rahmaninejad <rahmanin@discovery1.hpc.usc.edu>
2025-03-17 14:01:09 -0700

    adding plot for ctcf chipseq from occupancy

--- OccupancyInputCTCF/utils/plots.py
+++ OccupancyInputCTCF/utils/plots.py
@@ -22,7 +22,17 @@ RAD21['mid']=(RAD21.end+RAD21.start)/2
 bw_rad21 = pyBigWig.open(exp.bw_path_rad21)
 bw_ctcf = pyBigWig.open(exp.bw_path_ctcf)
 
-def plot_chip_hic(region, chip, binned_matrix, lattice_size = 0.25, output_file='plots.png'):
+def set_axes_ary(region, lattice_size=250, res = 100000):
+    region_ = bioframe.parse_region_string(region)
+    ### array to show in the map
+    mb = 1e6
+    start_reg = region_[1]
+    end_reg = region_[2]
+    ary = list(np.arange(0, (end_reg-start_reg)//(lattice_size)+1, res//lattice_size)) 
+    ary_str = [str((elements/mb)) for elements in np.arange(start_reg, end_reg+1,res)]
+    return ary, ary_str
+
+def plot_chip_hic(region, chip, chip_ctcf, binned_matrix, lattice_size = 0.25, res= 100000, output_file='plots.png'):
     """
     Hi-C and ChIP-seq plots along with simulated data visualizations.
     
@@ -83,22 +93,31 @@ def plot_chip_hic(region, chip, binned_matrix, lattice_size = 0.25, output_file=
 
     # Simulated Hi-C map
     ax = axs[0, 2]
-    im = ax.matshow(np.log10(binned_matrix + 1), cmap='fall')
-    ax.set_title('1D Simulation')
+    im = ax.matshow(np.log10(binned_matrix + 0.01),  cmap='fall')
+    ax.set_title('1D Simulation, %s'%region_[0])
     cax = axs[0, 3]
     plt.colorbar(im, cax=cax, label='raw counts')
 
     # Simulated extruder data
     ax1 = axs[1, 2]
-    ax1.plot(np.arange(len(chip)) * lattice_size, chip / np.sum(chip))
-    ax1.set_xlabel('extruders position, bp')
+    ax1.plot(np.arange(len(chip)) , chip / np.sum(chip)) #* lattice_size
+    ax1.set_xlabel('extruders position, mbp')
     ax1.set_title('Simulation, Extruder')
+    ax1.set_xlim(0, len(chip))
+    xticks, xticklabels = set_axes_ary(region, lattice_size=250, res = res)
+    ax1.set_xticks(xticks)
+    ax1.set_xticklabels(xticklabels, ha="right")  
     axs[1, 3].set_visible(False)
 
+
     ax1 = axs[2, 2]
-    ax1.plot(np.arange(len(chip)) * lattice_size, chip / np.sum(chip), color='red')
+    ax1.plot(np.arange(len(chip_ctcf)), chip_ctcf / np.sum(chip_ctcf), color='red')
     ax1.set_xlabel('extruders position')
-    ax1.set_title('Simulation')
+    ax1.set_title('Simulation, CTCF')
+    ax1.set_xlim(0, len(chip_ctcf))
+    xticksa, xticklabelsa = set_axes_ary(region, lattice_size=250, res = res)
+    ax1.set_xticks(xticksa)
+    ax1.set_xticklabels(xticklabelsa, ha="right") 
     axs[2, 3].set_visible(False)
 
     # Save and show plot
