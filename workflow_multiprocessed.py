9965e93d788210640d3c54da1f5a7f893eec3855
Hadi Rahmaninejad <rahmanin@discovery2.hpc.usc.edu>
2025-03-27 14:36:48 -0700

    revising 1d contact map calculations

--- workflow_multiprocessed.py
+++ workflow_multiprocessed.py
@@ -17,7 +17,7 @@ import OccupancyInputCTCF.utils.ml as ml
 import OccupancyInputCTCF.utils.makeparams as params
 import OccupancyInputCTCF.utils.One_d_simulation as simulation
 import OccupancyInputCTCF.utils.experimental_path as exp
-import OccupancyInputCTCF.utils.utils_s as utils_s
+import OccupancyInputCTCF.utils.cmap_utils as utils_s
 # ================ INPUT PARAMETERS =====================
 # Define genomic region and data files
 region = 'chr1:36000000-37500000'
@@ -25,7 +25,7 @@ region = 'chr1:36000000-37500000'
 # Simulation parameters
 with open('data/paramdict.json', 'r') as json_file:
     paramdict = json.load(json_file)
-
+#paramdict['CTCF_backstall']=[0.3]
 # Flags for simulation
 run_md_sim = True  # Flag for MD simulation
 use_predicted_occupancy = True  # Use predicted occupancy
@@ -34,7 +34,7 @@ input_occupancy = exp.ctcf_peaks  # Input occupancy file
 # =================== WORKFLOW ===========================
 print("Starting workflow...")
 
-# Step 1: Preprocessing
+# Step 1: Preprocessing bed files in selected region
 print("Step 1: Preprocessing...")
 ctcf_bed_df = snp.get_region_snippet(exp.ctcf_peaks, exp.ctcf_motifs, region)
 ctcf_bed_file = f'processing/{region}.csv'
@@ -64,8 +64,8 @@ print("Step 4 complete. Right barriers:", CTCF_right_positions, "Left barriers:"
 import multiprocessing as mp
 from functools import partial
 print("Step 5: Running 1D Simulation...")
-output_path = 'simulations/sims_a/'
-trajectory_length = 31500
+output_path = 'simulations/sims_c/'
+trajectory_length = 1950
 n = 5 # number of simulations in multiprocessing
 file_name = params.paramdict_to_filename(paramdict)
 output_directory = output_path + 'folder_' + file_name.split('file_')[1]
@@ -75,12 +75,12 @@ paramdict['monomers_per_replica'] = convert.get_lattice_size(region, lattice_sit
 ctcf_params = convert.get_ctcf_list(refined_occupancy, paramdict)
 #lef_positions = simulation.Perform_1d_simulation(paramdict, ctcf_params, trajectory_length, output_directory)
 
-outputt_dirs = []
+output_dirs = []
 for sim_id in range(1, n+1):
     file_name = f"simulation_{sim_id}"
     output_directory_partial = os.path.join(output_directory, f"{file_name}")
     os.makedirs(output_directory_partial, exist_ok=True)
-    outputt_dirs.append(output_directory_partial)
+    output_dirs.append(output_directory_partial)
     
 def Perform_1d_simulation(output_directory, paramdict, ctcf_params, trajectory_length):
     return simulation.Perform_1d_simulation(paramdict, ctcf_params, trajectory_length, output_directory)
@@ -92,7 +92,7 @@ def worker_init_fn():
     np.random.seed(int.from_bytes(os.urandom(4), byteorder='little'))
 with mp.Pool(processes=mp.cpu_count(), initializer=worker_init_fn) as pool:
     pool.map(
-        partially_set_Perform_1d_simulation, outputt_dirs
+        partially_set_Perform_1d_simulation, output_dirs
     )
     
 print("Step 5 complete. Output at", output_path)
@@ -110,6 +110,7 @@ mapN = paramdict['monomers_per_replica']*paramdict['sites_per_monomer']
 lattice_size = 0.25 # in kb
 lef_array = []
 n = 5
+lef_array = []
 wmap = []
 wchip = []
 wchip_ctcf = []
@@ -120,6 +121,7 @@ for sim_id in range(1, n+1):
     if os.path.exists(lef_file_path):
         print(f"LEFPositions.h5 file found at {lef_file_path}.")
         lefs = h5py.File(lef_file_path, 'r')['positions']
+        lef_array.append(lefs)
         print('length of lefs is %s'%len(np.array(lefs)))
         chip = utils_s.chip_seq_from_lef(lefs, mapN)
         cmap = utils_s.contact_map_from_lefs(lefs[:], mapN)
@@ -133,26 +135,44 @@ for sim_id in range(1, n+1):
         wchip_ctcf.append(np.array(ctcfchip))
     else:
         print(f"Error: LEFPositions.h5 file not found at {lef_file_path}")
+lefs_array = np.vstack(lef_array)
 
+# Multiprocessing for contact maps
+### creating approximated contact map from graph based distance
+# Directory setup
+map_output_directory = './data/contact_maps/'
+map_output_dirs = utils_s.create_contact_map_folders(5, map_output_directory)
 
-# Process and visualize contact map
-whole_map = np.sum(wmap, axis=0)
+str_frames = [10, 20, 30, 40, 50]  # Different starting frames
+end_frame = len(lefs_array)-80
+every_frame = 50
+max_dist = 150
+res_convert = 40
+replication_number = 10
+def worker(map_output_dir, str_frame):
+    utils_s.calculate_contact_map_save(lefs_array, str_frame, end_frame, every_frame, max_dist, res_convert, replication_number, map_output_dir)
+
+with mp.Pool(processes=mp.cpu_count()) as pool:
+    pool.starmap(worker, zip(map_output_dirs, str_frames))
+
+w_map = []
+for dirs in map_output_dirs:
+    file_path = os.path.join(dirs, 'contact_map.npz')
+    with np.load(file_path) as data:
+        cmap = data['contact_map']  
+        w_map.append(cmap)
+whole_map = np.sum(w_map, axis=0)
 whole_chip = np.sum(wchip, axis=0)
 whole_chip_ctcf = np.sum(wchip_ctcf, axis=0)    
 chip = utils_s.chip_seq_from_lef(lefs, mapN)
-
 cmap = whole_map
-matrix = adaptive_coarsegrain(cmap+1, cmap+1, cutoff = 3, max_levels=3, min_shape = 2)
-padded_matrix = np.pad(matrix, ((0, 1), (0, 1)), mode='constant')
-dimension = 100 #dimension of the final matrix
-binned_matrix = padded_matrix.reshape(dimension, len(padded_matrix)//dimension, dimension, len(padded_matrix)//dimension).sum(axis=(1, 3))
 print("Step 7 complete. Output at", output_path)
 
-# step 8: making maps 
+
+# step 8: plots 
 print("Step8: making experimental and simulated maps ...")
-output_file = 'newplots_%s_trial.pdf'%region
-#mplot.plot_chip_hic(region, whole_chip, binned_matrix, output_file=output_file)
-mplot.plot_chip_hic(region, whole_chip, whole_chip_ctcf, binned_matrix, res= 200000, output_file=output_file)
+output_file = 'outputs/newplots_%s_trial_back_.pdf'%region
+mplot.plot_chip_hic(region, whole_chip, whole_chip_ctcf, whole_map, res= 200000, output_file=output_file)
 print("Step 8 complete. Output at", output_file)
 
 # step 9: calculating FRiP
