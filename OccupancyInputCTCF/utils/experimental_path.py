c25af4160ec809a957e6b717c1a56f39ffe03908
Hadi Rahmaninejad <rahmanin@discovery1.hpc.usc.edu>
2025-04-24 15:46:02 -0700

    adding cool files from Li

--- OccupancyInputCTCF/utils/experimental_path.py
+++ OccupancyInputCTCF/utils/experimental_path.py
@@ -1,5 +1,7 @@
 # Path to the sample Hi-c map
 bonev_file = '/project/fudenber_735/GEO/bonev_2017_GSE96107/distiller-0.3.1_mm10/results/coolers/HiC_ES.mm10.mapq_30.1000.mcool'
+Li_file_wt = '/project2/fudenber_735/rahmanin/projects/Hi-C/results/coolers_library_group/WT.hg38.mapq_30.1000.mcool'
+Li_file_nterms = '/project2/fudenber_735/rahmanin/projects/Hi-C/results/coolers_library_group/Y226A.hg38.mapq_30.1000.mcool'
 # Path to the bigWig file
 bw_path_rad21 = "/project/fudenber_735/GEO/justice_2020_GSE137272/mm10/GSE137272_Rad21-ChIPseq.bw"
 bw_path_ctcf = "/project/fudenber_735/GEO/justice_2020_GSE137272/mm10/GSE137272_CTCF-ChIPseq.bw"
