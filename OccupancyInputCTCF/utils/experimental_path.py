60ec1ed159bbe41b38761d7a4957db361cabd973
Hadi Rahmaninejad <rahmanin@discovery1.hpc.usc.edu>
2025-09-16 13:33:57 -0700

    Updating for directories of experimental paths

--- OccupancyInputCTCF/utils/experimental_path.py
+++ OccupancyInputCTCF/utils/experimental_path.py
@@ -8,7 +8,7 @@ bw_path_ctcf = "/project2/fudenber_735/GEO/justice_2020_GSE137272/mm10/GSE137272
 ###importing ctcf peaks ####
 ctcf_path='/project2/fudenber_735/collaborations/karissa_2022/2022_09_features_for_RNAseq/ChIP-seq_in_WT-parental-E14'
 ### importing ctcf motifs ###
-motif_directory='/project/fudenber_735/motifs/mm10/jaspar/MA0139.1.tsv.gz'
+motif_directory='/project2/fudenber_735/motifs/mm10/jaspar/MA0139.1.tsv.gz'
 ### importing RAD21 
 RAD21_path=ctcf_path+'/RAD21_peaks_called_on_6reps_foundInatLeast3reps_noBlacklist.bed'#.query(f'chrom in {clr.chromnames}')
 ctcf_motifs = '/project2/fudenber_735/motifs/mm10/jaspar/MA0139.1.tsv.gz'
